136ade602c54c4712cb7f94e2c4b3c27dd6907d2
farid ahmed <farid@tgalimited.com>
2018-09-24 14:23:53 +0600

    update app js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
 // JS Goes here - ES6 supported
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+
+var axios = require('axios');
+
+//axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.baseURL = 'https://myrestapiworkd.cfapps.io/en/api/v1.0';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
+
+axios.get('/welcome')
+.then(function(respose){
+	alert(respose);
+})
+.catch(function (error) {
+   console.log(error);
+});
